82efa9a94239e705d7a5f62ad1535ed08c64378f
sans-byte <sanskarj15@gmail.com>
2024-08-26 15:11:44 +0530

    getting started with assets

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Productivity Landing Page",
+  description: "Landing page for a productivity application",
 };
 
 export default function RootLayout({
