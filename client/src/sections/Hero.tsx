119848f2829bdaeea29a5e128478e93cf55d7066
sans-byte <sanskarj15@gmail.com>
2024-08-27 20:01:11 +0530

    added animation for hero

--- client/src/sections/Hero.tsx
+++ client/src/sections/Hero.tsx
@@ -1,11 +1,20 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import Gear from "@/assets/Gear.png";
 import Image from "next/image";
 import Screw from "@/assets/Screw.png";
 import Bulb from "@/assets/Light Bulb.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="section-gradient-bg">
+    <section ref={heroRef} className="section-gradient-bg">
       <div className="container md:flex items-center">
         <div className="md:flex-1">
           <div className="text-badge">Version 3.0 is here</div>
@@ -19,33 +28,47 @@ export const Hero = () => {
           </p>
           <div className="flex mt-8 gap-3 items-center">
             <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text flex items-center gap-1">
-              <span>Learn more</span>
-              <ArrowRight className="h-5 w-5" />
-            </button>
           </div>
         </div>
         <div className="md:h-[648px] md:flex-1 relative">
-          <Image
-            src={Gear}
+          <motion.img
+            src={Gear.src}
             alt="gear"
             height={1000}
             width={1000}
             className="rotate-[-45deg] md:absolute md:h-full md:w-auto md:max-w-none md:-left-20 lg:left-0"
+            transition={{
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "mirror",
+              ease: "easeInOut",
+            }}
+            animate={{
+              translateY: [-30, 30],
+            }}
+            style={{
+              rotate: -40,
+            }}
           />
-          <Image
-            src={Screw}
+          <motion.img
+            src={Screw.src}
             alt="screw"
             height={200}
             width={200}
             className="rotate-[-120deg] hidden md:block"
+            style={{
+              translateY: translateY,
+            }}
           />
-          <Image
-            src={Bulb}
+          <motion.img
+            src={Bulb.src}
             alt="screw"
             height={300}
             width={300}
             className="absolute bottom-[-140px] right-[-80px] hidden md:block"
+            style={{
+              translateY: translateY,
+            }}
           />
         </div>
       </div>
